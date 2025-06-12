ee6e6557c7e15df2c5fec3cf80e65dfb8cad0547
FathurRahman0 <rahmanfthr165@gmail.com>
2025-06-12 17:53:56 +0700

    mebuat fungsi REAR akan kembali ke awal jika posisi di terakhir

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -35,6 +35,15 @@ public:
             FRONT = 0; // 2.a
             REAR = 0; // 2.b
         }
-    
-    }
+        else
+        {
+          // JIka REAR berada di posisi terakhir array, kembali ke awal array
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+            queues_array[REAR] = num;
+        }
+           
 }
