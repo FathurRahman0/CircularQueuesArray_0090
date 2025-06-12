a5c9665f581ad23cbd4fb20b520b877bb0fc2121
FathurRahman0 <rahmanfthr165@gmail.com>
2025-06-12 17:49:46 +0700

    membuat fungsi mengecek apakah antrian kosong

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -28,6 +28,13 @@ public:
             cout << "\nQueues overlow\n"; // 1.a
             return;                       // 1.b
         }
+
+        // 2. Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0; // 2.a
+            REAR = 0; // 2.b
+        }
     
     }
 }
